cf723be77b501538740b96d3c172a75c96e0c54e
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-05-15 19:06:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+---
+title: Herbário Cluster by Embeddings
+emoji: 🐔🏘️
+colorFrom: blue
+colorTo: pink
+sdk: gradio
+sdk_version: 5.0.2
+app_file: app.py
+pinned: false
+---
---
+++ app.py
@@ -0,0 +1,43 @@
+import gradio as gr
+import json
+import numpy as np
+
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+# from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import euclidean_distances
+
+with open("./20250515_embeddings.json", "r") as ifp:
+  data = json.load(ifp)
+
+ids = np.array(list(data.keys()))
+clip_embeddings = np.array([v["clip"] for v in data.values()])
+siglip_embeddings = np.array([v["siglip2"] for v in data.values()])
+
+def tsne_kmeans(emb_raw, n_clusters=8, n_components=3, perplexity=30):
+  mTSNE = TSNE(n_components=n_components, perplexity=perplexity, random_state=10)
+  mCluster = KMeans(n_clusters=n_clusters, random_state=1010)
+
+  emb_reduced = mTSNE.fit_transform(StandardScaler().fit_transform(emb_raw))
+  emb_clusters = mCluster.fit_predict(emb_reduced)
+
+  return emb_reduced, emb_clusters, mCluster.cluster_centers_
+
+def get_cluster_data(n_clusters):
+  embs, clusters, centers = tsne_kmeans(clip_embeddings, n_clusters=n_clusters)
+  cluster_distances = euclidean_distances(embs, centers)
+
+  i_c_d = zip(ids.tolist(), clusters.tolist(), cluster_distances.tolist())
+  return {id: {"cluster": c, "distances": d} for  id,c,d in i_c_d}
+
+with gr.Blocks() as demo:
+  gr.Interface(
+    fn=get_cluster_data,
+    inputs="number",
+    outputs="json",
+    allow_flagging="never",
+  )
+
+if __name__ == "__main__":
+   demo.launch()
